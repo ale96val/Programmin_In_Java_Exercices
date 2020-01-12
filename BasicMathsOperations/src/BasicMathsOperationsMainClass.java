3224a088a785839f18d3893a62678d970e06ab0d
Alejandro V <ale.valcarcel.ins@gmail.com>
2020-01-12 17:09:10 +0100

    Second exercise

---
+++ BasicMathsOperations/src/BasicMathsOperationsMainClass.java
@@ -0,0 +1,13 @@
+public class BasicMathsOperationsMainClass {  //Class
+    public static void main(String[] args) {  //Main Class
+        int n1 = 10;  //Variable for first value.
+        int n2 = 2;  //Variable for second value.
+        //Maths operations:
+        System.out.println("N1 + N2 = "+(n1+n2));
+        System.out.println("N1 - N2 = "+(n1-n2));
+        System.out.println("N1 * N2 = "+(n1*n2));
+        System.out.println("N1 / N2 = "+(n1/n2));
+        System.out.println("N1 % N2 = "+(n1%n2));
+        System.out.println("N1 + 1 = "+(++n1));
+    }
+}
